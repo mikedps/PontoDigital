050ce563079b6be872567c5658a27bb453bcb08a
Mike Pires <mikedps@gmail.com>
2022-11-25 15:45:58 -0300

    first-commit

---
+++ PontoDigital.Worker/Program.cs
@@ -0,0 +1,10 @@
+using PontoDigital.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
